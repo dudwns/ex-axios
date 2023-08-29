b861a7e091c4cf854464acd8436bcdc7c33b654f
yeongjun Kim <kyjun519@gmail.com>
2023-08-29 18:45:30 +0900

    feat: 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,8 @@ function App() {
     <DefaultTemplate>
       <Routes>
         <Route path="/" element={<h1>Home</h1>}></Route>
-      </Routes>
-      <Routes>
         <Route path="/posts" element={<PostsPage />}></Route>
-      </Routes>
-      <Routes>
         <Route path="/posts/:postId" element={<PostPage />}></Route>
-      </Routes>
-      <Routes>
         <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </DefaultTemplate>
